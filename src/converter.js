61c6ddd26dafbb737cce9c7e39e5bce079f3d414
Aram Kocharyan <akarmenia@gmail.com>
2014-12-21 01:56:35 +1100

    Renamed global export.

--- src/converter.js
+++ src/converter.js
@@ -4,7 +4,7 @@
   } else if (typeof exports === 'object') {
     module.exports = factory();
   } else {
-    root.returnExports = factory();
+    root.UtmConverter = factory();
   }
 }(this, function () {
 
